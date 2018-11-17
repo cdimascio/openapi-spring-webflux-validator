994b6c3ee6dd917752827a6948d3610801cbbb13
Carmine DiMascio <dimascio@us.ibm.com>
2018-11-17 16:24:53 -0500

    update doc

--- src/main/kotlin/io/github/cdimascio/swagger/Validate.kt
+++ src/main/kotlin/io/github/cdimascio/swagger/Validate.kt
@@ -8,62 +8,75 @@ import org.springframework.web.reactive.function.server.ServerResponse
 import reactor.core.publisher.Mono
 
 operator fun Regex.contains(text: CharSequence): Boolean = this.matches(text)
+
+/**
+ * Represents an error when validating a request against the
+ * Swagger 2 or OpenApi 3 specification
+ */
 data class ValidationError(val code: Int, val message: String)
 
+/**
+ * Handler for validation errors
+ */
 typealias ErrorHandler<T> = (status: HttpStatus, List<String>) -> T
+
+/**
+ * Validates requests against a Swagger 2 or OpenAPI 3 specification.
+ */
 class Validate<out T> internal constructor(
         swaggerJsonPath: String,
         errorHandler: ErrorHandler<T>) {
 
     /**
-     * Returns an instance of Validate
+     * The validate instance
      */
     companion object Instance {
         private val defaultErrorHandler: ErrorHandler<ValidationError> =
                 { status, messages -> ValidationError(status.value(), messages[0]) }
 
         /**
-         * Configure the validator by specifying the path to a Swagger v2 specification
-         * @param swaggerJsonPath path to a json formatted v2 swagger specification
-         * @return the validate object
+         * Configures the Validator with the Swagger 2 or OpenApi specification located at [openApiSwaggerPath]
+         * The Swagger 2 or OpenApi 3 specification file may be represented as YAML or JSON.
          */
-        fun configure(swaggerJsonPath: String) = configure(swaggerJsonPath, defaultErrorHandler)
+        fun configure(openApiSwaggerPath: String) = configure(openApiSwaggerPath, defaultErrorHandler)
 
         /**
-         * Configure the validator by specifying the path to a Swagger v2 JSON specification and a custom error handler
-         * @param swaggerJsonPath path to a json formatted v2 swagger specification
-         * @param errorHandler custom validation error handler
-         * @return the validate object
+         * Configures the Validator with the Swagger 2 or OpenApi specification located at [openApiSwaggerPath]
+         * and a custom [errorHandler]. The specification file may be represented as YAML or JSON.
          */
-        fun <T> configure(swaggerJsonPath: String, errorHandler: ErrorHandler<T>) = Validate(swaggerJsonPath, errorHandler)
+        fun <T> configure(openApiSwaggerPath: String, errorHandler: ErrorHandler<T>) = Validate(openApiSwaggerPath, errorHandler)
     }
 
     private val validator = Validator(swaggerJsonPath, errorHandler)
 
     /**
-     * @param request the server request, typically used withBody
-     * @return Request
+     * The [request] to validate
      */
     fun request(request: ServerRequest) = Request(request)
 
     /**
-     * @param handler a function which returns a server response
-     * @returns a server response
+     * Validates the [request]. If validation succeeds, the [handler] function is called to return a response
      */
     fun request(request: ServerRequest, handler: () -> Mono<ServerResponse>) = validator.validate(request) ?: handler()
 
     inner class Request(val request: ServerRequest) {
         /**
-         * @param bodyType the type to deserialize
-         * @param handler a function which recieves the body and returns a server response
-         * @returns a server response
+         * Validates a request with body of type [bodyType] . If validation succeeds, the [handler]
+         * is called to return a response
          */
         fun <T> withBody(bodyType: Class<T>, handler: (T) -> Mono<ServerResponse>): Mono<ServerResponse> {
             return BodyValidator(request, bodyType).validate(handler)
         }
     }
 
+
+    /**
+     * Creates a new BodyValidator to validate a [request] of type [bodyType]
+     */
     inner class BodyValidator<T>(val request: ServerRequest, val bodyType: Class<T>) {
+        /**
+         * Validates the body and calls [handler] if the validation succeeds
+         */
         fun validate(handler: (T) -> Mono<ServerResponse>): Mono<ServerResponse> {
             val success = { json: String -> jacksonObjectMapper().readValue(json, bodyType) }
             val json = request.body(BodyExtractors.toMono(String::class.java))
